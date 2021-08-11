432bcf40651ac1aa13ebed472723f5f53a90a701
roshin <imroshin@gmail.com>
2021-08-11 15:06:36 +0530

    hh config updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,15 @@
  * 
  */
  require('@nomiclabs/hardhat-ethers');
+ const { alchemyApiKey, mnemonic } = require('./secrets.json');
+
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+           rinkeby: {
+           url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+           accounts: { mnemonic: mnemonic },
+        },
+      },
 };
